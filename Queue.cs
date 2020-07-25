2f14ab9b68f76d1b23fe24d797741accdb4a2e34
farqtech <ikramf90@gmail.com>
2020-07-26 02:14:10 +0530

    stack & Queue

---
+++ Queue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructSandBox.DataStructures
+{
+    class Queue<T>: IEnumerable
+    {
+        private LinkedList<T> ll = new LinkedList<T>();
+
+        public bool IsEmpty() => this.ll.Count == 0;
+
+        public int Size() => this.ll.Count;
+
+        public T peeek()
+        {
+            if (this.IsEmpty())
+                throw new NullReferenceException();
+            return this.ll.Last.Value;
+        }
+
+        public void Enqueue(T data)
+        {
+            this.ll.AddFirst(data);
+        }
+
+        public T Dequeue()
+        {
+            if (this.IsEmpty())
+                throw new NullReferenceException();
+
+           T data =  this.ll.Last.Value;
+            this.ll.RemoveLast();
+            return data;
+        }
+
+        public IEnumerator GetEnumerator() => this.ll.GetEnumerator();
+    }
+
+    ////// driver 
+    ////public class Program
+    ////{
+    ////    public static int Main()
+    ////    {
+    ////        Queue<int> q = new Queue<int>();
+
+    ////        q.Enqueue(1);
+    ////        q.Enqueue(9);
+    ////        q.Enqueue(96);
+    ////        q.Enqueue(5);
+    ////        q.Enqueue(75);
+
+    ////        foreach (int i in q)
+    ////            Console.Write("{0} ", i);
+    ////        // output : 75 5 96 9 1
+
+    ////        return 0;
+    ////    }
+    ////}
+}
+
---
+++ Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructSandBox.DataStructures
+{
+    class Stack<T> : IEnumerable
+    {
+       private  LinkedList<T> ll = new LinkedList<T>();
+
+        public Stack()
+        {
+
+        }
+
+        public int Size() => this.ll.Count;
+
+        public bool IsEmpty() => this.ll.Count == 0;
+
+        public T Pop()
+        {
+            if (this.IsEmpty())
+                throw new NullReferenceException();
+            T data = this.ll.First.Value;
+            this.ll.RemoveFirst();
+            return data;
+        }
+
+        public void Push(T data)
+        {
+            this.ll.AddFirst(data);
+        }
+
+        public T Top()
+        {
+            return this.ll.First.Value;
+        }
+
+        public IEnumerator GetEnumerator() => this.ll.GetEnumerator();
+    }
+
+    ////// driver 
+    ////public class Program
+    ////{
+    ////    public static int Main()
+    ////    {
+    ////        Stack<int> st = new Stack<int>();
+
+    ////        st.Push(1);
+    ////        st.Push(8);
+    ////        st.Push(6);
+    ////        st.Push(7);
+
+    ////       foreach(int i in st)
+    ////        {
+    ////            Console.Write("{0} ", i);
+    ////        }
+
+    ////        // output: 7 6 8 1
+    ////        // last value is at the top
+
+    ////        return 0;
+    ////    }
+    ////}
+}
